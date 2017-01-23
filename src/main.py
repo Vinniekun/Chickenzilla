dc7a83cd9850b4ecda28b94ba73e67d8f919ad63
Vinnie <vdreifke@inf.ufsm.br>
2017-01-23 07:06:18 -0200

    finished2

--- src/main.py
+++ src/main.py
@@ -2,16 +2,10 @@ from engine import System
 from TestScene import TestScene
 from PongScene import PongScene
 from TextureTestScene import TextureTestScene
-#from Beach import Beach
 from StartScreen import StartScreen
-#from GameOver import GameOver
+
 
 system = System()
-#scene = TestScene()
-#scene = PongScene()
-#scene = TextureTestScene()
 scene = StartScreen()
-#scene = Beach()
-#scene = GameOver()
 system.push_scene(scene)
-system.run() # entra no laço principal do jogo
+system.run()
